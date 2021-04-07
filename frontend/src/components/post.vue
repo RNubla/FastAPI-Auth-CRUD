f02d0d2a943ae0f5ed37c2df480f29aa84257e99
RNubla <rnubla97@gmail.com>
2021-04-07 11:56:17 -0400

    added tailwind css

---
+++ frontend/src/components/post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- title -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    body: String,
+    author: String,
+  },
+};
+</script>
+
+<style></style>
