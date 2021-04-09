4f1d44882929f320680ce432ca165671ecd369a7
RNubla <rnubla97@gmail.com>
2021-04-09 15:13:31 -0400

    added local network

--- server/main.py
+++ server/main.py
@@ -2,3 +2,4 @@ import uvicorn
 
 if __name__ == '__main__':
     uvicorn.run('api.app:app', host='0.0.0.0', port=8000, reload=True)
+    # uvicorn.run('api.app:app', host='192.168.1.200', port=8000, reload=True)
