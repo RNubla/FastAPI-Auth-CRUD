eae3804a1cfe9d86fdbed8447f338b7d1e6d4f74
RNubla <rnubla97@gmail.com>
2021-04-12 16:08:25 -0400

    adding post push to different new-post route

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <button
       @click.prevent="togglePost"
       :class="{ active: isActive }"
@@ -8,7 +8,23 @@
       Add Posts
     </button>
     <post-entry v-if="isActive" />
-    <ul>
+    <ul> -->
+      <!-- <li v-for="post in getAllPosts.slice().reverse()" :key="post"> -->
+      <!-- <li v-for="post in gettersAllPosts" :key="post">
+        <post-card
+          :title="post.title"
+          :author="post.author"
+          :published_on="post.published_on"
+        >
+          {{ post.body.substring(0, 200) + "..." }}
+        </post-card>
+      </li>
+    </ul>
+  </div> -->
+  <div>
+    <router-link to="/new-post">Add New Post</router-link>
+    <div>
+      <ul>
       <!-- <li v-for="post in getAllPosts.slice().reverse()" :key="post"> -->
       <li v-for="post in gettersAllPosts" :key="post">
         <post-card
@@ -20,18 +36,19 @@
         </post-card>
       </li>
     </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import PostCard from "../components/PostCard.vue";
-import PostEntry from "../components/PostEntry.vue";
+// import PostEntry from "../components/PostEntry.vue";
 export default {
   name: "Home",
   components: {
     // HelloWorld,
-    PostEntry,
+    // PostEntry,
     PostCard,
   },
 
@@ -59,6 +76,7 @@ export default {
     togglePost() {
       this.isActive = !this.isActive;
     },
+
   },
   created() {
     this.getAllPosts();
