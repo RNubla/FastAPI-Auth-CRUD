16e0a4bb93a4e6e00276ea63ab085d78b24eb561
RNubla <rnubla97@gmail.com>
2021-04-12 15:57:02 -0400

    added styling

--- frontend/src/components/LoginRegistration/Login.vue
+++ frontend/src/components/LoginRegistration/Login.vue
@@ -7,7 +7,7 @@
 
         <div class="m-4">
             <form id="app" @submit.prevent="login" method="post">
-                <p v-if="errors.length">
+                <p class="mb-8" v-if="errors.length">
                     <b>Please correct the following error(s):</b>
                     <ul>
                     <li v-for="error in errors" :key="error">{{ error }}</li>
@@ -22,14 +22,14 @@
                     <input class="pl-2 h-10 shadow-md inline-block align-baseline placeholder-gray-500 text-left text-lg bg-gray-100 border border-transparent focus:ouylin-none focus:ring-2 focus:ring-green-200 rounded-md focus:border-transparent" type="password" placeholder="Password" minlength="3" v-model="loginInputs.password" />
                 </p>
                 <p class="m-8">
-                    <input class="bg-green-200 py-2 px-10 rounded-md" type="submit" value="Log In" />
+                    <input class="bg-green-300 py-2 px-10 rounded-md shadow-md" type="submit" value="Log In" />
                 </p>
             </form>
         </div>
       </div>
     <div class="bg-gradient-to-r from-green-400 to-green-500 flex justify-center">
         <div class="flex items-center  ">
-            <div>
+            <div class="px-8">
                 <p class="text-3xl uppercase font-semibold overflow-clip text-white">Log In into your account!</p>
                 <p class="font-thin text-white">Post anything you want</p>
             </div>
@@ -88,14 +88,6 @@ export default {
         } else {
           alert("Login failed");
         }
-        // this.autoRefreshToken();
-        // setTimeout(this.loginUser(), 3000);
-        // setInterval(this.refreshToken(), 3000);
-        // this.refreshToken()
-        // this.loginUserInputs.user_name = null;
-        // this.loginInputs.user_name = null;
-        // this.loginUserInputs.password = null;
-        // this.loginInputs.password = null;
       }
     },
   },
