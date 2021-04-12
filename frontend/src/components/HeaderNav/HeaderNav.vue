b30f3e75fd94d7c94b1ca9fd877841406cc92ef2
RNubla <rnubla97@gmail.com>
2021-04-12 15:56:25 -0400

    added logout and display username

--- frontend/src/components/HeaderNav/HeaderNav.vue
+++ frontend/src/components/HeaderNav/HeaderNav.vue
@@ -1,24 +1,54 @@
 <template>
-  <div class="grid grid-flow-col">
-    <div class="flex justify-start items-center">
-      <div class="m-2">Website Name</div>
+  <div class="grid grid-flow-row sm:grid-flow-col ">
+    <div class="flex justify-center items-center sm:justify-start">
+      <div class="m-2"><router-link to="/">Website Name</router-link></div>
     </div>
-    <div class="flex justify-end items-center">
-      <router-link class="m-2 p-3 shadow-md hover:bg-gray-50" to="/login"
-        ><p class="text-black text-center">Login</p></router-link
-      >
-
-      <router-link
-        class="m-2 bg-green-300 p-3 shadow-md hover:bg-green-400"
-        to="/register"
-        ><p class="text-black text-center">Sign Up</p></router-link
-      >
+    <div v-if="getLoginStatus == false">
+      <div class="flex justify-center items-center sm:justify-end">
+        <router-link class="m-2 p-3 shadow-md hover:bg-gray-50" to="/login"
+          ><p class="text-black text-center">Login</p></router-link
+        >
+        <router-link
+          class="m-2 bg-green-300 p-3 shadow-md hover:bg-green-400"
+          to="/register"
+          ><p class="text-black text-center">Sign Up</p></router-link
+        >
+      </div>
+    </div>
+    <div v-else>
+      <div class="flex justify-center items-center sm:justify-end">
+        <router-link class="m-2 p-3 shadow-md hover:bg-gray-50" to="/login"
+          ><p class="text-black text-center">
+            {{ getAuthData.user_name }}
+          </p></router-link
+        >
+        <div
+          @click="logoutUser"
+          class="m-2 bg-red-300 p-3 shadow-md hover:bg-red-400"
+          to="/logout"
+        >
+          <p class="text-black text-center font-bold">Logout</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapActions, mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters("auth", {
+      getLoginStatus: "getLoginStatus",
+      getAuthData: "getAuthData",
+    }),
+  },
+  methods: {
+    ...mapActions("auth", {
+      logoutUser: "logoutUser",
+    }),
+  },
+};
 </script>
 
 <style>
