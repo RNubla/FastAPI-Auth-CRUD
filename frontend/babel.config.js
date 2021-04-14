761623b39d4204c2d882b33f8bbe3b5afcd93544
RNubla <rnubla97@gmail.com>
2021-04-14 16:39:25 -0400

    added babel package

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,9 @@
+const removeConsole = [];
+if (process.env.NODE_ENV == "production") {
+  removeConsole.push("transform-remove-console");
+}
+
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: removeConsole,
 };
