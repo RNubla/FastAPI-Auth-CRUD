c80e19c083fa897ea511b8b6176fb08af5673ab9
RNubla <rnubla97@gmail.com>
2021-04-15 11:32:53 -0400

    Update EditPost.vue
    Moved getSinglePost to View

--- frontend/src/views/PostEntries/EditPost.vue
+++ frontend/src/views/PostEntries/EditPost.vue
@@ -1,15 +1,21 @@
 <template>
   <!-- <edit-post /> -->
-  <edit-post-v-2 />
+  <edit-post-v-2 :data="getSinglePost.data" />
 </template>
 
 <script>
 // import EditPost from "../components/Post/EditPost";
 import EditPostV2 from "../../components/Post/EditPost-v2";
+import { mapGetters } from "vuex";
 export default {
   components: {
     // EditPost,
     EditPostV2,
+  },
+  computed: {
+    ...mapGetters("posts", {
+      getSinglePost: "getSinglePost",
+    }),
   },
 };
 </script>
