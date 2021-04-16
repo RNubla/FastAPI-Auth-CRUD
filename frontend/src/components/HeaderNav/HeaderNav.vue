fb532b8a91ab189f47d5b0870af94196710cf877
RNubla <rnubla97@gmail.com>
2021-04-16 13:11:50 -0400

    changed name of home button

--- frontend/src/components/HeaderNav/HeaderNav.vue
+++ frontend/src/components/HeaderNav/HeaderNav.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-flow-row sm:grid-flow-col">
     <div class="flex justify-center items-center sm:justify-start">
       <div class="m-2">
-        <button @click.prevent="goToHomePage">Website Name</button>
+        <button @click.prevent="goToHomePage">Fast API Auth CRUD Demo</button>
       </div>
     </div>
     <div v-if="getLoginStatus == false || getLoginStatus == null">
