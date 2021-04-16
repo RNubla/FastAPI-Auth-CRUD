dc17380f514cb21196f9233dfe502ab93e9c491e
RNubla <rnubla97@gmail.com>
2021-04-16 13:00:48 -0400

    used starlette for cors

--- server/api/app.py
+++ server/api/app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Depends, Request
-from fastapi.middleware.cors import CORSMiddleware
+# from starlette import middleware
+# from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 # from fastapi.middleware.gzip import GZipMiddleware
 from api.db.users.database import auth_handler
 
@@ -11,27 +14,29 @@ from fastapi_jwt_auth.exceptions import AuthJWTException
 from .routes.users.authentication_users_routes import router as AuthRouter
 # from api.routes.posts.posts_routes import post_router as PostRouter
 from api.routes.posts.posts_routes_v2 import post_router as PostRouter
-app = FastAPI()
 
 origins = [
     # "http://localhost.tiangolo.com",
     # "https://localhost.tiangolo.com",
-    # 'https://rnubla.github.io/FastAPI-Auth-CRUD/',
-    # 'https://rnubla.github.io/',
-    '*',
+    'https://rnubla.github.io/FastAPI-Auth-CRUD/',
+    'https://rnubla.github.io/',
+    # '*',
     # "http://localhost:8000",
     # "http://localhost:8081",
     # "http://localhost:8080",
     # 'http://192.168.1.233:8080/'
 ]
+middleware = [Middleware(CORSMiddleware, allow_origin=origins)]
 # app.add_middleware(GZipMiddleware)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+app = FastAPI(middleware=middleware)
 
 
 app.include_router(AuthRouter, tags=['Authentication'], prefix='/auth')
