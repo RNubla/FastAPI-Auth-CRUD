1dbe71b9827829a1c41a37e01df9592a66492c73
RNubla <rnubla97@gmail.com>
2021-04-16 13:05:52 -0400

    cors

--- server/api/app.py
+++ server/api/app.py
@@ -27,7 +27,12 @@ origins = [
     # 'http://192.168.1.233:8080/'
 ]
 middleware = [
-    Middleware(CORSMiddleware, allow_origins=origins)
+    Middleware(CORSMiddleware,
+               allow_origins=origins,
+               allow_credentials=True,
+               allow_methods=["*"],
+               allow_headers=["*"],
+               )
 ]
 # app.add_middleware(GZipMiddleware)
 # app.add_middleware(
