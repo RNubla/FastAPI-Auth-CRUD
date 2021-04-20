49e4860af7b8b276bd6723c4f22a0e95441a2b05
RNubla <rnubla97@gmail.com>
2021-04-20 12:09:18 -0400

    fixed some styling issues

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,42 +4,27 @@
       <hero />
     </div>
     <div>
-      <div>Post</div>
-    </div>
-    <div>
       <router-link v-if="getLoginStatus == true" to="/new-post"
         >Add New Post</router-link
       >
-      <div class="px-5 mx-5">
-        <div class="w-full h-auto">
-          <div class="grid grid-rows-3 grid-flow-col gap-4">
-            <div class="col-span-2 bg-red-100">1</div>
-            <div class="row-span-2 col-span-2 bg-red-400">2</div>
-            <div class="row-span-3 bg-red-500">3</div>
-          </div>
-          <!-- <ul class="grid md:grid-cols-3"> -->
-          <ul class="flex flex-row">
-            <!-- <li v-for="post in getAllPosts.slice().reverse()" :key="post"> -->
-            <li
-              v-for="post in gettersAllPosts"
-              :key="post"
-              class="object-contain"
+      <!-- Post cards -->
+      <div class="mx-2 my-10 border-t-2">
+        <!-- <ul class="grid md:grid-cols-3"> -->
+        <ul class="mt-10 flex flex-row">
+          <!-- <li v-for="post in getAllPosts.slice().reverse()" :key="post"> -->
+          <li v-for="post in gettersAllPosts" :key="post" class="mx-8">
+            <post-card
+              class="w-72 border-2 border-gray-100"
+              :title="post.data.blocks[0].data.text"
+              :author="post.author"
+              :published_on="post.published_on"
+              :post_id="post.id"
+              :user_id="post.user_id"
+              :post_data="post.data.blocks"
             >
-              <post-card
-                class="w-auto"
-                :title="post.data.blocks[0].data.text"
-                :author="post.author"
-                :published_on="post.published_on"
-                :post_id="post.id"
-                :user_id="post.user_id"
-              >
-                <!-- {{ post.body.substring(0, 200) + "..." }} -->
-                <!-- {{ post.id }} -->
-                <!-- {{ post.body }} -->
-              </post-card>
-            </li>
-          </ul>
-        </div>
+            </post-card>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
