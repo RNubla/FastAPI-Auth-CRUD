dfaf0bb6af970f5d12d3a6b9ce3055ea9d9778db
RNubla <rnubla97@gmail.com>
2021-04-20 12:08:40 -0400

    change title of each card

--- frontend/src/components/Hero/Hero.vue
+++ frontend/src/components/Hero/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="my-5 font-bold text-3xl mx-auto">Recent Posts</div>
+  <div class="my-10 font-bold text-3xl mx-auto border-b-2">Recent Posts</div>
   <div class="sm:h-auto md:h-auto">
     <div
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 h-full px-5 my-5"
@@ -25,22 +25,22 @@ export default {
     return {
       carousel: {
         box1: {
-          title: "Hello",
+          title: "Welcome",
           img:
             "https://images.unsplash.com/photo-1612392062631-94dd858cba88?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
         },
         box2: {
-          title: "Bye",
+          title: "To",
           img:
             "https://images.unsplash.com/photo-1618598477738-bf2dc2c09bed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
         },
         box3: {
-          title: "Sike",
+          title: "The",
           img:
             "https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80",
         },
         box4: {
-          title: "About",
+          title: "Demo",
           img:
             "https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
         },
