139259e1faa9a456222454b765966e4bd5d7bb3b
RNubla <rnubla97@gmail.com>
2021-04-28 12:45:55 -0400

    adding padding to the whole app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -42,11 +42,13 @@ a {
   text-decoration: none;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Courier New", Courier, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 0 1em 0 1em;
 }
 
 #nav {
