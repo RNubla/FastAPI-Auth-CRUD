7c626dddd65cb973a0ad434f1bc5b016a1008601
RNubla <rnubla97@gmail.com>
2021-04-28 17:15:29 -0400

    implemented new post using tiptap

--- frontend/src/components/PostV2/NewPost.vue
+++ frontend/src/components/PostV2/NewPost.vue
@@ -1,41 +1,73 @@
 <template>
   <div>
-    <div class="grid grid-cols-6 gap-0" v-if="editor">
+    <div class="grid" v-if="editor">
       <button
         @click="editor.chain().focus().toggleBold().run()"
         :class="{ 'is-active': editor.isActive('bold') }"
       >
-        bold
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M8 11h4.5a2.5 2.5 0 1 0 0-5H8v5zm10 4.5a4.5 4.5 0 0 1-4.5 4.5H6V4h6.5a4.5 4.5 0 0 1 3.256 7.606A4.498 4.498 0 0 1 18 15.5zM8 13v5h5.5a2.5 2.5 0 1 0 0-5H8z"
+          />
+        </svg>
       </button>
       <button
         @click="editor.chain().focus().toggleItalic().run()"
         :class="{ 'is-active': editor.isActive('italic') }"
       >
-        italic
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path d="M15 20H7v-2h2.927l2.116-12H9V4h8v2h-2.927l-2.116 12H15z" />
+        </svg>
       </button>
       <button
         @click="editor.chain().focus().toggleStrike().run()"
         :class="{ 'is-active': editor.isActive('strike') }"
       >
-        strike
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M17.154 14c.23.516.346 1.09.346 1.72 0 1.342-.524 2.392-1.571 3.147C14.88 19.622 13.433 20 11.586 20c-1.64 0-3.263-.381-4.87-1.144V16.6c1.52.877 3.075 1.316 4.666 1.316 2.551 0 3.83-.732 3.839-2.197a2.21 2.21 0 0 0-.648-1.603l-.12-.117H3v-2h18v2h-3.846zm-4.078-3H7.629a4.086 4.086 0 0 1-.481-.522C6.716 9.92 6.5 9.246 6.5 8.452c0-1.236.466-2.287 1.397-3.153C8.83 4.433 10.271 4 12.222 4c1.471 0 2.879.328 4.222.984v2.152c-1.2-.687-2.515-1.03-3.946-1.03-2.48 0-3.719.782-3.719 2.346 0 .42.218.786.654 1.099.436.313.974.562 1.613.75.62.18 1.297.414 2.03.699z"
+          />
+        </svg>
       </button>
       <button
         @click="editor.chain().focus().toggleCode().run()"
         :class="{ 'is-active': editor.isActive('code') }"
       >
-        code
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M23 12l-7.071 7.071-1.414-1.414L20.172 12l-5.657-5.657 1.414-1.414L23 12zM3.828 12l5.657 5.657-1.414 1.414L1 12l7.071-7.071 1.414 1.414L3.828 12z"
+          />
+        </svg>
       </button>
       <button @click="editor.chain().focus().unsetAllMarks().run()">
         clear marks
       </button>
       <button @click="editor.chain().focus().clearNodes().run()">
         clear nodes
-      </button>
-      <button
-        @click="editor.chain().focus().setParagraph().run()"
-        :class="{ 'is-active': editor.isActive('paragraph') }"
-      >
-        paragraph
       </button>
       <button
         @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
@@ -89,7 +121,17 @@
         @click="editor.chain().focus().toggleCodeBlock().run()"
         :class="{ 'is-active': editor.isActive('codeBlock') }"
       >
-        code block
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M4 18v-3.7a1.5 1.5 0 0 0-1.5-1.5H2v-1.6h.5A1.5 1.5 0 0 0 4 9.7V6a3 3 0 0 1 3-3h1v2H7a1 1 0 0 0-1 1v4.1A2 2 0 0 1 4.626 12 2 2 0 0 1 6 13.9V18a1 1 0 0 0 1 1h1v2H7a3 3 0 0 1-3-3zm16-3.7V18a3 3 0 0 1-3 3h-1v-2h1a1 1 0 0 0 1-1v-4.1a2 2 0 0 1 1.374-1.9A2 2 0 0 1 18 10.1V6a1 1 0 0 0-1-1h-1V3h1a3 3 0 0 1 3 3v3.7a1.5 1.5 0 0 0 1.5 1.5h.5v1.6h-.5a1.5 1.5 0 0 0-1.5 1.5z"
+          />
+        </svg>
       </button>
       <button
         @click="editor.chain().focus().toggleBlockquote().run()"
@@ -105,14 +147,91 @@
       </button>
       <button @click="editor.chain().focus().undo().run()">undo</button>
       <button @click="editor.chain().focus().redo().run()">redo</button>
+      <button
+        @click="editor.chain().focus().setTextAlign('left').run()"
+        :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M3 4h18v2H3V4zm0 15h14v2H3v-2zm0-5h18v2H3v-2zm0-5h14v2H3V9z"
+          />
+        </svg>
+      </button>
+      <button
+        @click="editor.chain().focus().setTextAlign('center').run()"
+        :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M3 4h18v2H3V4zm2 15h14v2H5v-2zm-2-5h18v2H3v-2zm2-5h14v2H5V9z"
+          />
+        </svg>
+      </button>
+      <button
+        @click="editor.chain().focus().setTextAlign('right').run()"
+        :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M3 4h18v2H3V4zm4 15h14v2H7v-2zm-4-5h18v2H3v-2zm4-5h14v2H7V9z"
+          />
+        </svg>
+      </button>
+      <button
+        @click="editor.chain().focus().setParagraph().run()"
+        :class="{ 'is-active': editor.isActive('paragraph') }"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M12 6v15h-2v-5a6 6 0 1 1 0-12h10v2h-3v15h-2V6h-3zm-2 0a4 4 0 1 0 0 8V6z"
+          />
+        </svg>
+      </button>
+      <button @click="addImage">Image</button>
     </div>
+
     <editor-content :editor="editor" />
+    <div type="button" @click="submitPost" class="">
+      <div class="text-2xl font-bold">Submit</div>
+    </div>
   </div>
 </template>
 
 <script>
 import { Editor, EditorContent } from "@tiptap/vue-3";
 import { defaultExtensions } from "@tiptap/starter-kit";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import TextAlign from "@tiptap/extension-text-align";
+import Text from "@tiptap/extension-text";
+import CodeBlock from "@tiptap/extension-code-block";
+import Dropcursor from "@tiptap/extension-dropcursor";
+import Image from "@tiptap/extension-image";
+import { mapActions } from "vuex";
 
 export default {
   components: {
@@ -122,12 +241,56 @@ export default {
   data() {
     return {
       editor: null,
+      json: null,
+      data: null,
+      inputPost: {
+        data: null,
+        published_on: new Date(),
+      },
     };
   },
 
+  methods: {
+    ...mapActions("posts", {
+      addPost: "addPost",
+    }),
+    addImage() {
+      const url = window.prompt("URL");
+      if (url) {
+        this.editor.chain().focus().setImage({ src: url }).run();
+      }
+    },
+    async submitPost() {
+      console.log("SAVING");
+      this.inputPost.data = this.json;
+      console.log(this.inputPost);
+      await this.addPost(this.inputPost);
+      this.$router.push({
+        name: "Home",
+      });
+    },
+  },
   mounted() {
     this.editor = new Editor({
-      extensions: [...defaultExtensions()],
+      extensions: [
+        ...defaultExtensions(),
+        Document,
+        Paragraph,
+        Text,
+        TextAlign,
+        CodeBlock,
+        Image,
+        Dropcursor,
+      ],
+      editorProps: {
+        attributes: {
+          spellchekc: "false",
+        },
+      },
+    });
+    this.json = this.editor.getJSON();
+    this.editor.on("update", () => {
+      this.json = this.editor.getJSON();
     });
   },
 
@@ -136,3 +299,67 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+/* Basic editor styles */
+.grid {
+  display: grid;
+  grid-template-columns: repeat(11, minmax(0, 1fr));
+  grid-auto-flow: dense;
+}
+.ProseMirror {
+  > * + * {
+    // margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(hsl(0, 0%, 38%), 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: hsl(0, 0%, 33%);
+    color: #fff;
+    font-family: "JetBrainsMono", monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0d0d0d, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0d0d0d, 0.1);
+    margin: 2rem 0;
+  }
+}
+</style>
