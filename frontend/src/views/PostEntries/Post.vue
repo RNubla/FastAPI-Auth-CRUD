c488ac8effce8d1117e751b18995909c1a102169
RNubla <rnubla97@gmail.com>
2021-04-28 17:19:44 -0400

    setting view post

--- frontend/src/views/PostEntries/Post.vue
+++ frontend/src/views/PostEntries/Post.vue
@@ -5,16 +5,19 @@
     :published_on="getSinglePost.published_on"
     :author="getSinglePost.author"
   /> -->
-  <post
+  <!-- <post
     :author="getSinglePost.author"
     :published_on="getSinglePost.published_on"
     :data="getSinglePost.data"
-  />
+  /> -->
+  <post :body="getSinglePost" />
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import Post from "../../components/Post/Post";
+// import Post from "../../components/Post/Post.vue";
+// import Post from "../../components/Post/Post";
+import Post from "../../components/PostV2/ViewPost";
 export default {
   components: {
     Post,
