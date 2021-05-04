8f63a2bd80c9011dc5142c0593c7427bf6e87276
RNubla <rnubla97@gmail.com>
2021-05-04 11:05:02 -0400

    Added brand name to footer
    Footer added issue #9

--- frontend/src/components/Footer/V-Footer.vue
+++ frontend/src/components/Footer/V-Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="footer mt-auto py-3 bg-light">
     <div class="container py-4">
+      <div class="d-flex justify-content-center">
+        <p>FastAPI Auth CRUD</p>
+      </div>
       <div class="d-flex justify-content-evenly">
         <div class="container">
           <a href="https://pages.github.com/">GitHub Pages</a>
