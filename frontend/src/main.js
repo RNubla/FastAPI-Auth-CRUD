bbff19e8ea573855afd85efa50071458bb8c973b
RNubla <rnubla97@gmail.com>
2021-05-03 21:17:06 -0400

    import Bootstrap 5
    Proper import of bootstrap 5

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 // import "./assets/tailwind.css";
 // import "./assets/tailwind.css";
 // import * as appRouter from "./appRouter";
