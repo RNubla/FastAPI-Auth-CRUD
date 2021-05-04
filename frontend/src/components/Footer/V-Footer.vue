3fc470d12d6260ce7ede9e95d19f82ea2222b6b1
RNubla <rnubla97@gmail.com>
2021-05-04 10:59:53 -0400

    Added Footer
    Created and added footer at the bottom of the page issue#9

---
+++ frontend/src/components/Footer/V-Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="footer mt-auto py-3 bg-light">
+    <div class="container py-4">
+      <div class="d-flex justify-content-evenly">
+        <div class="container">
+          <a href="https://pages.github.com/">GitHub Pages</a>
+        </div>
+        <div class="container">
+          <a href="https://github.com/RNubla/FastAPI-Auth-CRUD">Source Code</a>
+        </div>
+        <div class="container">
+          <a href="https://github.com/RNubla">GitHub Profile</a>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center">
+        <a href="https://github.com/RNubla/FastAPI-Auth-CRUD/blob/main/LICENSE"
+          >MIT License</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
