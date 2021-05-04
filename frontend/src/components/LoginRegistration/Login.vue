b191fee6c1c040bd5b505e8cfde47865a4dc334f
RNubla <rnubla97@gmail.com>
2021-05-03 21:34:28 -0400

    Css to bootstrap
    Used bootstrap form

--- frontend/src/components/LoginRegistration/Login.vue
+++ frontend/src/components/LoginRegistration/Login.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="container">
+  <div class="container bg-light mt-5 pt-5 pb-5">
+    <p class="fw-bold fs-4">Login</p>
+    <form class="container needs-validation" @submit.prevent="login">
+      <div class="input-group flex-nowrap mb-3">
+        <!-- <label for="username" class="form-label">Username</label> -->
+        <span class="input-group-text" id="addon-usernameHelp">@</span>
+        <input
+          type="text"
+          class="form-control"
+          id="validationUsername"
+          aria-label="Username"
+          placeholder="Username"
+          aria-describedby="usernameHelp"
+          v-model="loginInputs.user_name"
+          required
+        />
+        <div class="invalid-feedback">Please enter a username</div>
+      </div>
+      <div class="input-group flex-nowrap mb-3">
+        <input
+          type="password"
+          placeholder="Password"
+          class="form-control"
+          id="exampleInputPassword1"
+          v-model="loginInputs.password"
+          required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Log In</button>
+    </form>
+  </div>
+  <!-- <div class="container">
       <div class="">
           <div class="m-4 pt-8">
               <p class="text-center font-extrabold text-3xl ">Login</p>
@@ -14,11 +46,9 @@
                     </ul>
                 </p>
                 <p class="mx-4 ">
-                    <!-- <input type="text" v-model="loginUserInputs.user_name" /> -->
                     <input class="pl-2 h-10 shadow-md inline-block align-baseline placeholder-gray-500 text-left text-lg bg-gray-100 border border-transparent focus:ouylin-none focus:ring-2 focus:ring-green-200 rounded-md focus:border-transparent" type="text" placeholder="Username" v-model="loginInputs.user_name" />
                 </p>
                 <p class="m-4">
-                    <!-- <input type="password" minlength="3" v-model="loginUserInputs.password" /> -->
                     <input class="pl-2 h-10 shadow-md inline-block align-baseline placeholder-gray-500 text-left text-lg bg-gray-100 border border-transparent focus:ouylin-none focus:ring-2 focus:ring-green-200 rounded-md focus:border-transparent" type="password" placeholder="Password" minlength="3" v-model="loginInputs.password" />
                 </p>
                 <p class="m-8">
@@ -35,7 +65,7 @@
             </div>
         </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -44,6 +74,7 @@ export default {
   data() {
     return {
       errors: [],
+      forms: document.querySelectorAll(".needs-validation"),
       storedUser: null,
       loginInputs: {
         user_name: null,
@@ -70,15 +101,15 @@ export default {
     async login() {
       this.storedUser = this.loginUserInputs;
 
-      this.errors = [];
-      // if (!this.loginUserInputs.user_name) {
-      if (!this.loginInputs.user_name) {
-        this.errors.push("Username required.");
-      }
-      // if (!this.loginUserInputs.password) {
-      if (!this.loginInputs.password) {
-        this.errors.push("Password required.");
-      }
+      // this.errors = [];
+      // // if (!this.loginUserInputs.user_name) {
+      // if (!this.loginInputs.user_name) {
+      //   this.errors.push("Username required.");
+      // }
+      // // if (!this.loginUserInputs.password) {
+      // if (!this.loginInputs.password) {
+      //   this.errors.push("Password required.");
+      // }
       if (this.errors.length === 0) {
         // console.log("Error is empty");
         // this.storedUser = this.loginUserInputs;
@@ -86,7 +117,7 @@ export default {
         if (this.getLoginStatus === true) {
           this.$router.push("/");
         } else {
-          alert("Login failed");
+          alert("Username or password might be incorrect");
         }
       }
     },
