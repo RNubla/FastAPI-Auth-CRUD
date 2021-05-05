9b0417f051510082f68ebae88868818ce18391b8
RNubla <rnubla97@gmail.com>
2021-05-05 16:11:21 -0400

    deploy

---
+++ precache-manifest.8c2ed9aa8e1179d0df7e3db8d3fa101c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730d38a4b93267b02c66bd078ec96c85",
+    "url": "/FastAPI-Auth-CRUD/404.html"
+  },
+  {
+    "revision": "c0beba9eba252168872f",
+    "url": "/FastAPI-Auth-CRUD/css/app.af7f5b42.css"
+  },
+  {
+    "revision": "dae6bb2d9372b70a8a53",
+    "url": "/FastAPI-Auth-CRUD/css/chunk-vendors.5d30aad5.css"
+  },
+  {
+    "revision": "5291593814bf3b8ec621f989f950e8dd",
+    "url": "/FastAPI-Auth-CRUD/index.html"
+  },
+  {
+    "revision": "c0beba9eba252168872f",
+    "url": "/FastAPI-Auth-CRUD/js/app.dfc9a0ed.js"
+  },
+  {
+    "revision": "dae6bb2d9372b70a8a53",
+    "url": "/FastAPI-Auth-CRUD/js/chunk-vendors.bfd7b5e5.js"
+  },
+  {
+    "revision": "58f8898d24e7ce9902d6f1be3490fa0f",
+    "url": "/FastAPI-Auth-CRUD/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/FastAPI-Auth-CRUD/robots.txt"
+  }
+]);
